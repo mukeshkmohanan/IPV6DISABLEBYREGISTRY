48734754776cef6a9e87354ca3f5074cde499398
mukesh mohanan <mukeshmohanan.k@gmail.com>
2020-07-06 11:23:13 +0530

    First Commit After Complete

---
+++ IPV6/frmIPV6Disable.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace WindowsFormsApplication13
+{
+    partial class frmIPV6Disable
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnRestart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(7, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(392, 55);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "IPV6 DISABLED";
+            // 
+            // btnRestart
+            // 
+            this.btnRestart.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRestart.ForeColor = System.Drawing.Color.MediumAquamarine;
+            this.btnRestart.Location = new System.Drawing.Point(127, 60);
+            this.btnRestart.Name = "btnRestart";
+            this.btnRestart.Size = new System.Drawing.Size(139, 32);
+            this.btnRestart.TabIndex = 1;
+            this.btnRestart.Text = "RESTART NOW";
+            this.btnRestart.UseVisualStyleBackColor = true;
+            this.btnRestart.Click += new System.EventHandler(this.btnRestart_Click);
+            // 
+            // frmIPV6Disable
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(403, 99);
+            this.Controls.Add(this.btnRestart);
+            this.Controls.Add(this.label1);
+            this.Name = "frmIPV6Disable";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "IPV6 DISABLE";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnRestart;
+    }
+}
+
---
+++ IPV6/frmIPV6Disable.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.OracleClient;
+using System.Threading;
+using Microsoft.Win32;
+using Microsoft.Win32.SafeHandles;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Drawing.Drawing2D;
+using System.Drawing.Printing;
+using System.Diagnostics;
+
+
+namespace WindowsFormsApplication13
+{
+    public partial class frmIPV6Disable : Form
+    {
+        public frmIPV6Disable()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+
+                Microsoft.Win32.RegistryKey key = Microsoft.Win32.Registry.LocalMachine.CreateSubKey(@"SYSTEM\CurrentControlSet\Services\TCPIP6\Parameters");
+
+                key.SetValue("DisabledComponents", unchecked((int)0xffffffff), RegistryValueKind.DWord);
+
+                MessageBox.Show("Done, Please Restart The System ---- >>>> PRESS RESTART BUTTON FOR RESTART NOW ");
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Registry Disabled Error " + ex.ToString());
+                this.Dispose();
+            }
+        }
+
+        private void btnRestart_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Are you Sure want to Restart The System", "RESTART", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                ProcessStartInfo proc = new ProcessStartInfo();
+                proc.WindowStyle = ProcessWindowStyle.Hidden;
+                proc.FileName = "cmd";
+                proc.Arguments = "/C shutdown -f -r -t 5";
+                Process.Start(proc);
+            }
+        }
+    }
+}
